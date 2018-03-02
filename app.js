c4eb6afae9683ec9bc3aebc6a0bff0ca01afb6d1
feng <fre1994@gmail.com>
2018-03-02 15:32:52 -0700

    added route to post text to watson

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+const port = process.env.PORT || 4001;
+const routes = require("./routes/watson");
+const bodyParser = require('body-parser')
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(routes)
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
