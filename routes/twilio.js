fc286cf982d4865fdb2cbcd89a7b0c0322776375
feng <fre1994@gmail.com>
2018-03-09 12:16:04 -0700

    added authorization using dotenv

--- routes/twilio.js
+++ routes/twilio.js
@@ -3,56 +3,127 @@ const knex = require('../knex')
 const router = express.Router()
 // const watson = require('./watson')
 
+function testing (body,res) {
+  console.log(body);
+
+  const authorization_numbers = [process.env.FREDERICK_NUMBER, process.env.SCOTT_NUMBER]
+  const received_number = '+15038063822'
+  let authorized = false
+  console.log(body.From, authorization_numbers[0]);
+  for (let i in authorization_numbers) {
+    if (authorization_numbers[i] === received_number) {
+      authorized = true
+    }
+  }
+  if (authorized === false) {
+    return res.status(403).send(`Your number ${received_number} is not authorized on this misty server.`)
+  }
+  // Message is a string
+  let message = body.Body
+  message.includes('#voice')
+  if (message.includes("https://messages.alexa.amazon.com")){
+    message = message.split(`\nhttps://messages.alexa.amazon.com/`)[0]
+    console.log(message);
+  }
+  // // API call to watson routes
+  // let unirest = require("unirest");
+  //
+  // let request = unirest("POST", "http://localhost:3000/watson/receive");
+  //
+  // request.headers({
+  //   "Cache-Control": "no-cache",
+  //   "Content-Type": "application/json"
+  // });
+  //
+  // request.type("json");
+  // request.send({
+  //   "text": `${message}`
+  // });
+  //
+  // request.end(function (response) {
+  //   if (response.error) throw new Error(res.error);
+  //
+  //   console.log(response.body);
+  //   res.status(200).send("success")
+  // });
+}
+testing({ ToCountry: 'US',
+  ToState: 'OR',
+  NumMedia: '0',
+  ToCity: '',
+  FromZip: '97230',
+  FromState: 'OR',
+  SmsStatus: 'received',
+  FromCity: 'PORTLAND',
+  Body: 'Alexa\nhttps://messages.alexa.amazon.com/s/UvmVAW45xZnpzYlWqsB3nC',
+  FromCountry: 'US',
+  To: '+19718035516',
+  ToZip: '',
+  NumSegments: '1',
+  From: '+15038063822',
+  ApiVersion: '2010-04-01' })
+
 router.post('/twilio/receive', (req, res) => {
   console.log(req.body);
 
-  // Message is a string
-  let message = req.body.Body
+  /*
+    User Authorization
+  */
+  const authorization_numbers = [process.env.FREDERICK_NUMBER, process.env.SCOTT_NUMBER]
+  const received_number = req.body.From
+  let authorized = false
+  for (let i in authorization_numbers) {
+    if (authorization_numbers[i] === received_number) {
+      authorized = true
+    }
+  }
+  if (authorized === false) {
+    return res.status(403).send(`Your number ${received_number} is not authorized on this misty server.`)
+  }
+  /*
+    End of user Authorization
+  */
+
+/*
+  Alexa message url removal
+*/
+  let message = body.Body
   message.includes('#voice')
+  if (message.includes("https://messages.alexa.amazon.com")){
+    message = message.split(`\nhttps://messages.alexa.amazon.com/`)[0]
+    console.log(message);
+  }
+  /*
+    End Alexa message url removal
+  */
+
+  /*
+    #voice commands for watson synthesize
+  */
+
+
 
   // API call to watson routes
-  let request = require("request");
+  let unirest = require("unirest");
 
-  let options = {
-    method: 'POST',
-    url: 'http://localhost:3000/watson/receive',
-    headers: {
-      'Cache-Control': 'no-cache',
-      Authorization: 'Basic ZThiM2QwMjQtOGUyMC00YjU4LTg5MzQtZWZiZGI1NDk2MzIyOmdzeDI4R3FpWG1SSQ==',
-      Accept: 'audio/wav',
-      'Content-Type': 'application/json'
-    },
-    body: {
-      voice: `${selected_voice}`,
-      text: `${message}`
-    },
-    json: true
-  };
-  request(options, function(error, response, body) {
-    if (error) throw new Error(error);
+  let request = unirest("POST", "http://localhost:3000/watson/receive");
 
-    // console.log(body);
+  request.headers({
+    "Cache-Control": "no-cache",
+    "Content-Type": "application/json"
   });
-  res.status(200).send("success")
+
+  request.type("json");
+  request.send({
+    "text": `${message}`
+  });
+
+  request.end(function (response) {
+    if (response.error) throw new Error(res.error);
+
+    console.log(response.body);
+    res.status(200).send("success")
+  });
 })
-// { ToCountry: 'US',
-//   ToState: 'OR',
-//   SmsMessageSid: 'SMed3515cf69cb24fc7902601e7a254044',
-//   NumMedia: '0',
-//   ToCity: '',
-//   FromZip: '97230',
-//   SmsSid: 'SMed3515cf69cb24fc7902601e7a254044',
-//   FromState: 'OR',
-//   SmsStatus: 'received',
-//   FromCity: 'PORTLAND',
-//   Body: 'Yeah ',
-//   FromCountry: 'US',
-//   To: '+19718035516',
-//   ToZip: '',
-//   NumSegments: '1',
-//   MessageSid: 'SMed3515cf69cb24fc7902601e7a254044',
-//   AccountSid: 'ACa61dd227286ddc76fbe8e939398b2aab',
-//   From: '+15038063822',
-//   ApiVersion: '2010-04-01' }
 
 module.exports = router;
