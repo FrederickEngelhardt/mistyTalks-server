41f0a9973add271973185c11fa40e1871f7edc22
feng <fre1994@gmail.com>
2018-03-17 16:22:36 -0600

    db now points to proper foreign keys. Removed user_id and preferences_id

--- db/migrations/20180303184910_00_users.js
+++ db/migrations/20180303184910_00_users.js
@@ -2,12 +2,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', table => {
     table.increments();
-    table.integer('user_id').notNullable();
-    table.string('first_name').defaultTo('').notNullable();
-    table.string('last_name').defaultTo('').notNullable();
+    table.string('first_name').defaultTo('');
+    table.string('last_name').defaultTo('');
     table.string('email').notNullable().unique().comment('This is the email field');
     table.string('password').notNullable();
-
   })
 };
 
