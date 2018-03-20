df36933e1876e01469d189ac201b7132bc67fcf7
feng <fre1994@gmail.com>
2018-03-20 00:10:51 -0600

    removed functions to open a navbar on index.html

--- public/src/index.js
+++ public/src/index.js
@@ -38,12 +38,4 @@ createLoginListener();
     // },
     // complete: function() {}
   })
-  $('.button-collapse').sideNav({
-    menuWidth: 300, // Default is 300
-    edge: 'left', // Choose the horizontal origin
-    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    draggable: true,
-    // onOpen: function(el) {}
-    // onClose: function(el) {}
-  })
 })
