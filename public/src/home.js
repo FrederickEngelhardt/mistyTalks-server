eadecc84a9ba62f78f4bc62add891f9c70a1ab34
feng <fre1994@gmail.com>
2018-04-05 19:32:40 -0600

    css fixes for s5

--- public/src/home.js
+++ public/src/home.js
@@ -765,7 +765,7 @@ const editMistyPreferences_listener = () => {
 
     // Materialize listener
     $('.timepicker').pickatime({
-      default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+      default: '12:00', // Set default time: 'now', '1:30AM', '16:30'
       fromnow: 0, // set default time to * milliseconds from now (using with default = 'now')
       twelvehour: false, // Use AM/PM or 24-hour format
       donetext: 'OK', // text for done-button
